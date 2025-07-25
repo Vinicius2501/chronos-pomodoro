291c2acd43fc2b8931d2f0df5d491b7633ecdc4e
Vinicius Vasconcelos da Silva <vinicius.vasconcelos2501@gmail.com>
2025-07-25 19:39:13 -0300

    Adicionando footer + estilização

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { DefaultInput } from './components/DefaultInput';
 import { Cycles } from './components/Cycles';
 import { DefaultButton } from './components/DefaultButton';
 import { PlayCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer';
 
 export function App() {
   return (
@@ -33,9 +34,12 @@ export function App() {
             <Cycles />
           </div>
           <div className='formRow'>
-            <DefaultButton icon={<PlayCircleIcon />} color='green' />
+            <DefaultButton icon={<PlayCircleIcon />} />
           </div>
         </form>
+      </Container>
+      <Container>
+        <Footer />
       </Container>
     </>
   );
