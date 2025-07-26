6f9e3f0a27bd94ab3f2c3177be20b8685fb989c9
Vinicius Vasconcelos da Silva <vinicius.vasconcelos2501@gmail.com>
2025-07-26 14:16:30 -0300

    iniciando lógica de criar nova task

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,13 +2,60 @@ import { PlayCircleIcon } from 'lucide-react';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
+import { useRef } from 'react';
+import type { TaskModel } from '../../models/TaskModel';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 //import style from './style.module.css';
 
 export function MainForm() {
+  const { setState } = useTaskContext();
+  const taskNameImput = useRef<HTMLInputElement>(null);
+
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    if (taskNameImput.current === null) return;
+
+    const taskName = taskNameImput.current.value.trim();
+
+    if (!taskName) {
+      alert('Digite o nome da tarefa');
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      durationInMinutes: 1,
+      type: 'workTime',
+    };
+
+    const secondsRemaining = newTask.durationInMinutes * 60;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: 1,
+        secondsRemaining: secondsRemaining,
+        formattedSecondsRemaining: '00:00',
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
+  }
+
   return (
-    <form className='form' action=''>
+    <form onSubmit={handleCreateNewTask} className='form' action=''>
       <div className='formRow'>
-        <DefaultInput id='input' type='text' placeholder='Digite algo' />
+        <DefaultInput
+          id='input'
+          type='text'
+          placeholder='Digite algo'
+          ref={taskNameImput}
+        />
       </div>
       <div className='formRow'>
         <p>Lorem ipsum dolor sit amet.</p>
