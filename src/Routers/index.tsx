8b8fd5dc92f40a35c94f3f878d9afcf0569ddab5
Vinicius Vasconcelos da Silva <vinicius.vasconcelos2501@gmail.com>
2025-07-30 23:49:05 -0300

    feat: adicionando componente de configuração dos timers

--- src/Routers/index.tsx
+++ src/Routers/index.tsx
@@ -4,6 +4,7 @@ import { AboutPomodor } from '../pages/AboutPomodoro';
 import { NotFound } from '../pages/NotFound';
 import { useEffect } from 'react';
 import { History } from '../pages/History';
+import { Settings } from '../pages/Settings';
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -19,8 +20,9 @@ export function Routers() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/history' element={<History />} />
-        <Route path='about-pomodoro' element={<AboutPomodor />} />
+        <Route path='/history/' element={<History />} />
+        <Route path='/settings/' element={<Settings />} />
+        <Route path='/about-pomodoro/' element={<AboutPomodor />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
       <ScrollToTop />
